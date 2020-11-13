400fe86ed791fa5ddc232c36bd4769e5d9809556
Fabricio Kassardjian <fkay13@hotmail.com>
2020-11-13 00:28:26 -0300

    commit inicial

---
+++ app/src/main/java/com/example/desafio2/view/activity/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.desafio2.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.desafio2.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
