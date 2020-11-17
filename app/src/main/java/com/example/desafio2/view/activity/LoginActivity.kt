f81a321b5e0dc4e54d7f82b54879f3814169266b
Fabricio Kassardjian <fkay13@hotmail.com>
2020-11-16 22:45:55 -0300

    validacoes dos campos

--- app/src/main/java/com/example/desafio2/view/activity/LoginActivity.kt
+++ app/src/main/java/com/example/desafio2/view/activity/LoginActivity.kt
@@ -5,21 +5,66 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.example.desafio2.R
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tilLoginEmail: TextInputLayout
+    private lateinit var tilLoginSenha: TextInputLayout
+    private lateinit var btLogin: AppCompatButton
+    private lateinit var btLoginRegister: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<AppCompatButton>(R.id.btLogin).setOnClickListener() {
-            startActivity(Intent(this,HomeActivity::class.java))
+        initComponents()
 
-            // close this activity
-            finish()
+        setObservables()
+
+
+    }
+
+    private fun setObservables() {
+        btLogin.setOnClickListener() {
+            if(validateFields()) {
+                startActivity(Intent(this, HomeActivity::class.java))
+
+                // close this activity
+                finish()
+            }
         }
 
-        findViewById<AppCompatButton>(R.id.btRegister).setOnClickListener() {
-            startActivity(Intent(this,RegisterActivity::class.java ))
+        btLoginRegister.setOnClickListener() {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
+    }
+
+    private fun initComponents() {
+        tilLoginEmail = findViewById(R.id.tilLoginEmail)
+        tilLoginSenha = findViewById(R.id.tilLoginSenha)
+        btLogin = findViewById(R.id.btLogin)
+        btLoginRegister = findViewById(R.id.btLoginRegister)
+    }
+
+    private fun validateFields(): Boolean {
+        val email = tilLoginEmail.editText?.text.toString()
+        val senha = tilLoginSenha.editText?.text.toString()
+
+        var validate = true
+
+        if(email.isBlank()) {
+            tilLoginEmail.error = "Campo obrigatório"
+            validate = false
+        } else {
+            tilLoginEmail.isErrorEnabled = false
+        }
+
+        if(senha.isBlank()) {
+            tilLoginSenha.error = "Campo obrigatório"
+            validate = false
+        } else {
+            tilLoginSenha.isErrorEnabled = false
+        }
+        return validate
     }
 }
--- app/src/main/java/com/example/desafio2/view/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/desafio2/view/activity/RegisterActivity.kt
@@ -5,21 +5,84 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import com.example.desafio2.R
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var tilRegisterNome: TextInputLayout
+    private lateinit var tilRegisterEmail: TextInputLayout
+    private lateinit var tilRegisterSenha: TextInputLayout
+    private lateinit var tilRegisterRepeteSenha: TextInputLayout
+    private lateinit var btRegisterRegister: AppCompatButton
+
+    private val tilObrigatory: MutableList<TextInputLayout> = mutableListOf()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         this.supportActionBar?.title = "Register"
 
-        findViewById<AppCompatButton>(R.id.btRegisterRegister).setOnClickListener() {
-            val intent = Intent(this, HomeActivity::class.java)
 
-            // tira activity de login
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-            finish()
+        initComponents()
+
+        setupObservables()
+    }
+
+    private fun setupObservables() {
+        btRegisterRegister.setOnClickListener() {
+            if(validateFields()) {
+                val intent = Intent(this, HomeActivity::class.java)
+
+                // tira activity de login
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
+            }
         }
+    }
+
+    private fun validateFields():Boolean {
+        var validate = true
+        tilObrigatory.forEach {
+            validate = validate and checkObrigatoryField(it)
+        }
+
+        val senha = tilRegisterSenha.editText?.text.toString()
+        val repeteSenha = tilRegisterRepeteSenha.editText?.text.toString()
+
+        if (senha.isNotBlank() and repeteSenha.isNotBlank() and (senha != repeteSenha)) {
+            tilRegisterRepeteSenha.error = "Senhas devem ser iguais"
+            validate = false
+        }
+
+        return validate
+    }
+
+    private fun checkObrigatoryField(til: TextInputLayout): Boolean {
+        val texto = til.editText?.text.toString()
+
+        if(texto.isBlank()) {
+            til.error = "Campo obrigatório"
+            return false
+        } else {
+            til.isErrorEnabled = false
+            return true
+        }
+    }
+
+    private fun initComponents() {
+        tilRegisterEmail = findViewById(R.id.tilRegisterEmail)
+        tilRegisterNome = findViewById(R.id.tilRegisterNome)
+        tilRegisterSenha = findViewById(R.id.tilRegisterSenha)
+        tilRegisterRepeteSenha = findViewById(R.id.tilRegisterRepeteSenha)
+        btRegisterRegister = findViewById(R.id.btRegisterRegister)
+        tilObrigatory.addAll( listOf(
+            tilRegisterNome,
+            tilRegisterEmail,
+            tilRegisterSenha,
+            tilRegisterRepeteSenha,
+            )
+        )
     }
 }
